79b90ab2920787484d4ba9607826627e0e7be775
Ziziou91 <joshgray91@gmail.com>
2018-07-25 19:41:09 +0100

    create babel config

---
+++ js/ClientApp.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { render } from "react-dom";
+
+const ce = React.createElement;
+
+const MyTitle = function(props) {
+  return ce(
+    "div",
+    null,
+    ce("h1", { style: { color: props.color } }, props.title)
+  );
+};
+const MyFirstComponent = function() {
+  return ce(
+    "div",
+    { id: "my-frst-component" },
+    ce(MyTitle, { title: "Game of Thrones", color: "YellowGreen" }),
+    ce(MyTitle, { title: "Stanger Things", color: "GreenYellow" }),
+    ce(MyTitle, { title: "Breaking Bad", color: "LimeGreen" }),
+    ce(MyTitle, { title: "Rick & Morty", color: "Lime" })
+  );
+};
+
+render(ce(MyFirstComponent), document.getElementById("app"));
